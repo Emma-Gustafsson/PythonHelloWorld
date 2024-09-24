54df8e0c0beb954ebd474e1407cd88c1e989b928
Emma-Gustafsson <emma@cellfish.se>
2024-09-24 21:48:39 +0000

    Update hello_world.py by asking user for name

--- hello_world.py
+++ hello_world.py
@@ -1,10 +1,15 @@
-
+"""
+This is my first program.
+Emma Gustafsson - September 2024
+"""
 
 
 def main():
-  pass
+  name: str = input("What is your name? ")
+  print("Hello", name, "!")
 
 
 
 if __name__ == "__main__":
   main()
+
